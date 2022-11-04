562c85233dc870ac47c7bff91705dffd5413b8c3
madinarsp <madinarsp20@gmail.com>
2022-11-04 17:56:04 +0600

    feat: implement functionality for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fsPromisesModule = require("fs/promises");
+const path = require("path");
 
+const pathToOrigFolder = path.join(__dirname, "files");
+const pathToCopiedFolder = path.join(__dirname, "files-copy");
+
+async function copyFolderContent() {
+  await fsPromisesModule.mkdir(pathToCopiedFolder, { recursive: true });
+
+  const folderContent = await fsPromisesModule.readdir(pathToOrigFolder, {
+    withFileTypes: true,
+  });
+
+  const arrayOfFileNames = [];
+
+  for (const contentItem of folderContent) {
+    const fileName = contentItem.name;
+
+    const pathToOrigFile = path.join(pathToOrigFolder, fileName);
+    const pathToCopiedFile = path.join(pathToCopiedFolder, fileName);
+    fsPromisesModule.copyFile(pathToOrigFile, pathToCopiedFile);
+
+    arrayOfFileNames.push(fileName);
+  }
+
+  const copiedFolderContent = await fsPromisesModule.readdir(
+    pathToCopiedFolder,
+    {
+      withFileTypes: true,
+    }
+  );
+
+  for (const contentItem of copiedFolderContent) {
+    const fileName = contentItem.name;
+    const pathToFile = path.join(pathToCopiedFolder, fileName);
+    if (!arrayOfFileNames.includes(fileName)) {
+      fsPromisesModule.rm(pathToFile);
+    }
+  }
+}
+
+copyFolderContent();
